e7f4cf2d6f8660f7efab14ab680d748a0bae535e
Md-Anees-Alam <mohammadanees03@gmail.com>
2025-03-29 21:04:22 +0530

    updated test_calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,4 +1,10 @@
+import sys
 import pytest
+import os
+
+# Ensure the project root is in the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app.calculator import add, subtract, multiply, divide
 
 def test_add():
